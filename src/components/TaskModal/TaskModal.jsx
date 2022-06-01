e8c954ae5ce7a048424db82dbb1657a3f41d4db4
Devansu <devansuyadav@gmail.com>
2022-06-01 17:07:57 +0530

    fix: Added View Btn for tasks, fixed task modal

--- src/components/TaskModal/TaskModal.jsx
+++ src/components/TaskModal/TaskModal.jsx
@@ -1,44 +1,28 @@
-import { useState, useEffect } from "react";
 import { useTaskModal } from "../../common/context/TaskModalContext";
 import { useTaskData } from "../../common/context/TaskDataContext";
-import { CREATE_TASK, UPDATE_TASK, MIN_TASK_NAME_LENGTH, MIN_TASK_DESCRIPTION_LENGTH } from "../../common/constants";
+import { CREATE_TASK, UPDATE_TASK } from "../../common/constants";
 import { createTask, updateTask } from "../../common/helpers/TaskService";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import "./TaskModal.css";
 
 const TaskModal = () => {
     const { modalToggle, setModalToggle, taskModalData, setTaskModalData } = useTaskModal();
     const { taskDataDispatch } = useTaskData();
-    const [taskPropertiesError, setTaskPropertiesError] = useState({ taskName: false, taskDescription: false });
-    const taskNameLength = taskModalData.taskName.length;
-    const taskDescriptionLength = taskModalData.taskDescription.length;
 
     const onSubmitAddTaskHandler = async (event) => {
-        if(!taskPropertiesError.taskName && !taskPropertiesError.taskDescription) {
-            event.preventDefault();
-            const createTaskResponse = await createTask(taskModalData);
-            console.log("Task Created - ", createTaskResponse);
-            taskDataDispatch({ type: CREATE_TASK, payload: {...taskModalData, _id: createTaskResponse[createTaskResponse.length - 1]._id }});
-            setModalToggle(modal => ({...modal, displayModal: false }));
-        }
+        event.preventDefault();
+        const createTaskResponse = await createTask(taskModalData);
+        console.log("Task Created - ", createTaskResponse);
+        taskDataDispatch({ type: CREATE_TASK, payload: {...taskModalData, _id: createTaskResponse[createTaskResponse.length - 1]._id }});
+        setModalToggle(modal => ({...modal, displayModal: false }));
     }
 
     const onSubmitUpdateTaskHandler = async (event) => {
-        if(!taskPropertiesError.taskName && !taskPropertiesError.taskDescription) {
-            event.preventDefault();
-            const updateTaskResponse = await updateTask(taskModalData);
-            console.log("Task Updated - ", updateTaskResponse);
-            taskDataDispatch({ type: UPDATE_TASK, payload: taskModalData });
-            setModalToggle(modal => ({...modal, displayModal: false }));
-        }
+        event.preventDefault();
+        const updateTaskResponse = await updateTask(taskModalData);
+        console.log("Task Updated - ", updateTaskResponse);
+        taskDataDispatch({ type: UPDATE_TASK, payload: taskModalData });
+        setModalToggle(modal => ({...modal, displayModal: false }));
     }
-
-    // Checking if there are any errors or not.
-    useEffect(() => {
-        setTaskPropertiesError(taskError => taskNameLength >= MIN_TASK_NAME_LENGTH ? {...taskError, taskName: false}: {...taskError, taskName: true});
-        setTaskPropertiesError(taskError => taskDescriptionLength >= MIN_TASK_DESCRIPTION_LENGTH ? {...taskError, taskDescription: false}: {...taskError, taskDescription: true});
-    }, [taskNameLength, taskDescriptionLength]);
 
     return (
         <div>
@@ -50,42 +34,24 @@ const TaskModal = () => {
                     <img className="btn-close" src="/assets/images/close-button.svg" alt="Close Button" onClick={() => setModalToggle(modal => ({...modal, displayModal: false }))}/>
                 </div>
 
-                <form className={`flex-col-container task-form`}>
+                <form className={`flex-col-container task-form`} onSubmit={async(e) => {
+                    modalToggle.type === "add" ? await onSubmitAddTaskHandler(e): await onSubmitUpdateTaskHandler(e);
+                }}>
                     <div className='form-item'>
                         <label className="form-input-label" htmlFor="task-name">Task Name*</label>
                         <input type="text" id="task-name" className={`form-input-field input-primary`} value={taskModalData.taskName} name='taskName' placeholder='Add Title' 
                         onChange={(event) => {
                             setTaskModalData(taskItem => ({...taskItem, taskName: event.target.value}));
-                        }} required></input>
+                        }} required />
                     </div>
-                    { taskPropertiesError.taskName && 
-                        <div className='modal-error'>
-                            <FontAwesomeIcon icon={faXmark} />
-                            ({taskNameLength}/{ MIN_TASK_NAME_LENGTH })
-                        </div>
-                    }
-                    {
-                        !taskPropertiesError.taskName &&
-                        <div className="modal-success">Great going!</div>
-                    }
 
                     <div className='form-item'>
                         <label className="form-input-label" htmlFor="task-description">Task Description *</label>
                         <textarea id="task-description" className='form-text-area input-primary' value={taskModalData.taskDescription} name='taskDescription' rows="3" placeholder='Add Description' 
                         onChange={(event) => {
                             setTaskModalData(taskItem => ({...taskItem, taskDescription: event.target.value}));
-                        }} required></textarea>
+                        }} required />
                     </div>
-                    { taskPropertiesError.taskDescription && 
-                        <div className='modal-error'>
-                            <FontAwesomeIcon icon={faXmark} />
-                            ({taskDescriptionLength}/{ MIN_TASK_DESCRIPTION_LENGTH })
-                        </div>
-                    }
-                    {
-                        !taskPropertiesError.taskDescription &&
-                        <div className="modal-success">Great going!</div>
-                    }
 
                     <div className='form-item'>
                         <label className="form-input-label" htmlFor="focus-duration">Focus Duration *</label>
@@ -120,12 +86,8 @@ const TaskModal = () => {
                     <div className='form-actions flex-row-container'>
                         <input type="button" value="Cancel" className='btn btn-outline-primary rounded-med space-S' onClick={() => setModalToggle(modal => ({...modal, displayModal: false }))}></input>
                         
-                        { modalToggle.type === "add" ? <input type="submit" value="ADD TASK" className={`btn btn-outline-primary ${taskPropertiesError.taskName || taskPropertiesError.taskDescription ? "btn-disable": ""} rounded-med space-S`} onClick={async (event) => {
-                            await onSubmitAddTaskHandler(event);
-                        }}></input>
-                         : <input type="submit" value="UPDATE TASK" className={`btn btn-outline-primary ${taskPropertiesError.taskName || taskPropertiesError.taskDescription ? "btn-disable": ""} rounded-med space-S`} onClick={async(event) => {
-                            await onSubmitUpdateTaskHandler(event);
-                         }}></input>
+                        { modalToggle.type === "add" ? <input type="submit" value="ADD TASK" className={`btn btn-outline-primary rounded-med space-S`} />
+                         : <input type="submit" value="UPDATE TASK" className={`btn btn-outline-primary rounded-med space-S`} />
                         }
                     </div>
                 </form>
