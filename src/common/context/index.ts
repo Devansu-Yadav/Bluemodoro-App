54f0857ae16da2e8a803099b910c2d1513c8ee0e
Devansu <devansuyadav@gmail.com>
2022-07-03 18:45:26 +0530

    minor: re-factor code

--- src/common/context/index.ts
+++ src/common/context/index.ts
@@ -1,5 +1,2 @@
 export { useTaskData, TaskDataProvider } from "./TaskDataContext";
 export { useTaskModal, TaskModalToggle } from "./TaskModalContext";
-
-// Exporting Context level types
-// export { TaskContext, ModalToggle, TaskModalCtxt } from "./taskContext.types";
